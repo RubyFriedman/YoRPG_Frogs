f82357445d9bee9aa0cb6b2fe02d461def5e8026
samanthahuaa <samanthahua1@gmail.com>
2021-11-23 21:01:18 -0500

    v3

---
+++ v3/Knight.java
@@ -0,0 +1,11 @@
+public class Knight extends Protagonist {
+	public Knight() {
+		health = 100;
+		strength = 100;
+		defense = 55;
+		attack = 0.6;
+	}
+
+	public String getName() {return _name;}
+	public int getDefense() {return defense;}
+}
