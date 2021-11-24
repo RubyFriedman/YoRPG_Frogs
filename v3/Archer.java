7f9e5c559a9f0854136b7b938e6383b5cd0311f3
Ivina Wang <ivinaw@gmail.com>
2021-11-23 21:04:37 -0500

    more v3

---
+++ v3/Archer.java
@@ -0,0 +1,11 @@
+public class Archer extends Protagonist {
+	public Archer() {
+		health = 100;
+		strength = 150;
+		defense = 30;
+		attack = 0.4;
+	}
+
+	public String getName() {return _name;}
+	public int getDefense() {return defense;}
+}
